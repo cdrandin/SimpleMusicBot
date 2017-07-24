3e90d3c8fd0a68a204e1038a4f41daff994b440b
christopher randin <cdrandin@hotmail.com>
2017-07-24 15:50:21 -0700

    converted to class

--- music_bot.py
+++ music_bot.py
@@ -6,10 +6,8 @@ import os
 import sys
 import yaml
 
-from service.youtube import youtube_fetch
-
-ONCE = False
-CLIENT = discord.Client()
+# from service.youtube import youtube_fetch
+import service
 
 LOGGER = logging.getLogger(__file__)
 LOGGER.setLevel(logging.DEBUG)
@@ -21,93 +19,92 @@ with open(os.path.join(PROJECT_DIR, 'logging_config.yaml')) as infile:
     logging.config.dictConfig(D)
 
 
-def how_to(message):
-    s = \
-        '''
-    Simple Music Bot commands:
-        commands only work in `botcommands` channel
+class SimpleMusicBot(discord.Client):
 
-        `<>` are parameters `[<>]` are optional parameters
-        - `!smb_intro`: brings up this message
-        - `!youtube`: fetch youtube link of query
-            ex) `!youtube <query> [<channel>]`
-    '''
-    return CLIENT.send_message(message.channel, s)
+    @asyncio.coroutine
+    def on_ready(self):
+        LOGGER.info('Logged in as')
+        LOGGER.info(self.user.name)
+        LOGGER.info(self.user.id)
+        LOGGER.info('------')
+
+        self.bot_ch = self.get_botcommands_channel()
+        self.once = False
+        self.task = asyncio.Task(self.reset_once())
+        self.loop = asyncio.get_event_loop()
+
+    @asyncio.coroutine
+    def on_message(self, message):
+        if message.content.startswith('!smb_intro'):
+            yield from self.how_to(message)
+
+        elif message.content.startswith('!youtube'):
+            LOGGER.info('youtube')
+            if not self.once and not message.channel.name == 'botcommands':
+                yield from self.not_in_botcommands_channel_message(message)
+                return
+
+            yield from service.handle_youtube_fetch(message)
+
+    @asyncio.coroutine
+    def reset_once(self):
+        while True:
+            self.once = False
+            yield from asyncio.sleep(5)
+
+    def get_botcommands_channel(self):
+        bot_ch = None
+        for ch in self.get_all_channels():
+            if ch.name == 'botcommands':
+                bot_ch = ch
+                break
+
+        return bot_ch
+
+    @asyncio.coroutine
+    def how_to(self, message):
+        msg = \
+            '''
+            Simple Music Bot commands:
+                commands only work in `botcommands` channel
+
+                `<>` are parameters `[<>]` are optional parameters
+                - `!smb_intro`: brings up this message
+                - `!youtube`: fetch youtube link of query
+                    ex) `!youtube <query> [<channel>]`
+            '''
+        return self.send_message(message.channel, msg)
+
+    # NOTE: No sure if should be here or in the service __init__ scope. 
+    # @asyncio.coroutine
+    # def handle_youtube_fetch(self, message):
+    #     ch = message.channel_mentions[
+    #         0] if message.channel_mentions else message.channel
+    #     msg_list = message.content.split()
+    #
+    #     query = ' '.join(msg_list[1:])
+    #     query = query.replace('<#{}>'.format(ch.id), '')
+    #     LOGGER.debug(query)
+    #
+    #     res = youtube_fetch.fetch_youtube_query(query)
+    #     LOGGER.info(res)
+    #
+    #     if res['reason'] == '':
+    #         return self.send_message(ch, youtube_fetch.youtube_link(res['href']))
+    #     else:
+    #         return self.send_message(message.channel, res['reason'])
+
+    @asyncio.coroutine
+    def not_in_botcommands_channel_message(self, message):
+        self.once = True
+        bot_ch = self.botcommands_channel()
+        return self.send_message(message.channel, 'Please post only in {}.'.format(bot_ch.mention))
 
 
-def botcommands_channel():
-    # HACK: lazy
-    bot_ch = None
-    for ch in CLIENT.get_all_channels():
-        if ch.name == 'botcommands':
-            bot_ch = ch
-            break
-
-    return bot_ch
+def main():
+    smb = SimpleMusicBot()
+    smb.run(sys.argv[1])
 
 
-def in_botcommands_channel(message):
-    return message.channel.name == 'botcommands'
-
-
-def not_in_botcommands_channel_message(message):
-    ONCE = True
-    bot_ch = botcommands_channel()
-    return CLIENT.send_message(message.channel, 'Please post only in {}.'.format(bot_ch.mention))
-
-
-@asyncio.coroutine
-def reset_once():
-    # HACK: lazy
-    while True:
-        global ONCE
-        ONCE = False
-        yield from asyncio.sleep(5)
-
-task = asyncio.Task(reset_once())
-loop = asyncio.get_event_loop()
-
-
-def handle_youtube_fetch(message):
-    ch = message.channel_mentions[
-        0] if message.channel_mentions else message.channel
-    msg_list = message.content.split()
-
-    query = ' '.join(msg_list[1:])
-    query = query.replace('<#{}>'.format(ch.id), '')
-    LOGGER.debug(query)
-
-    res = youtube_fetch.fetch_youtube_query(query)
-    LOGGER.info(res)
-
-    if res['reason'] == '':
-        return CLIENT.send_message(ch, youtube_fetch.youtube_link(res['href']))
-    else:
-        return CLIENT.send_message(message.channel, res['reason'])
-
-
-@CLIENT.event
-async def on_ready():
-    LOGGER.info('Logged in as')
-    LOGGER.info(CLIENT.user.name)
-    LOGGER.info(CLIENT.user.id)
-    LOGGER.info('------')
-
-
-@CLIENT.event
-async def on_message(message):
-    # HACK: lazy
-    global ONCE
-
-    if message.content.startswith('!smb_intro'):
-        await how_to(message)
-
-    elif message.content.startswith('!youtube'):
-        LOGGER.info('youtube')
-        if not ONCE and not in_botcommands_channel(message):
-            await not_in_botcommands_channel_message(message)
-            return
-
-        await handle_youtube_fetch(message)
-
-CLIENT.run(sys.argv[1])
+if __name__ == '__main__':
+    main()
